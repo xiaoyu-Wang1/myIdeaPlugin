29052172426ff9e295369610ac5f3530d3f30dd8
GitHub Action <action@github.com>
2023-08-09 11:11:50 +0000

    Template cleanup

--- src/main/kotlin/com/github/xiaoyuwang1/myideaplugin/MyBundle.kt
+++ src/main/kotlin/com/github/xiaoyuwang1/myideaplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.xiaoyuwang1.myideaplugin
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/xiaoyuwang1/myideaplugin/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/xiaoyuwang1/myideaplugin/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.xiaoyuwang1.myideaplugin.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
